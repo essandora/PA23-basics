d5d869a97764137ba183ef7274cff202dc50a0b4
Sandra Lillepool <sandra.lillepool@voco.ee>
2023-09-19 13:52:41 +0300

    numbers.js

---
+++ numbers.js
@@ -0,0 +1,34 @@
+// data types
+// string 
+
+let firstname = 'Sandra'
+let lastname = 'Lillepool'
+
+console.log(firstname +''+ lastname)
+console.log(typeof firstname)
+
+//numbers
+let goodMark = '5'
+
+console.log(typeof goodMark)
+
+let percent = 55
+console.log(typeof percent)
+
+let isToday = true
+console.log(typeof isToday)
+
+//type conversions
+//number --> string
+console.log(typeof 5+5)
+console.log(5.5) + String(5.5)
+
+//boolean to string
+console.log[typeof String(true)]
+
+//string to number
+console.log[Number('666')]
+console.log[Number('66.6')]
+console.log(parseInt('183.3'))
+console.log(parseFloat('183.3'))
+
