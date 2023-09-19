136d5d6da2a6fb9d1a67f834c2cc5445dbcda02f
Sandra Lillepool <sandra.lillepool@voco.ee>
2023-09-19 12:03:28 +0300

    js variables - declare, assign and use

---
+++ variables.js
@@ -0,0 +1,20 @@
+//variables
+//keyword variablename=value
+//let
+
+let programmingLanguage='JavaScript'
+console.log(programmingLanguage)
+
+
+// assignment - save now value into variable
+
+programmingLanguage = 'PHP'
+console.log(programmingLanguage)
+
+let result
+console.log(result)
+
+//const
+const group = 'VS23'
+// group="PA23" not allowed
+console.log(group)
